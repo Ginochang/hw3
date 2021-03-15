97e8cc04319b02657b0f3a98bf026b8bd9967f13
ginochang <richlovegod@gmail.com>
2021-03-15 01:26:19 -0500

    3

--- firestore.js
+++ firestore.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', async function(event) {
   let db = firebase.firestore()
 
   // Step 1: Make the world's tiniest to-do app
-  document.querySelector('form').addEventListener('submit', async function(event) {
+  document.querySelector('.addInterest').addEventListener('submit', async function(event) {
     event.preventDefault()
 
     let todoText = document.querySelector('#todo').value
@@ -18,16 +18,17 @@ document.addEventListener('DOMContentLoaded', async function(event) {
 
       document.querySelector('.todos').insertAdjacentHTML('beforeend', `
         <div class="todo-${todoId} py-4 text-xl border-b-2 border-purple-500 w-full">
-          <a href="#" class="done p-2 text-sm bg-green-500 text-white">✓</a>
+          <a href="#" class="done p-2 text-sm bg-green-500 opacity-20 text-white">✓</a>
           ${todoText}
         </div>
       `)
 
       document.querySelector(`.todo-${todoId} .done`).addEventListener('click', async function(event) {
-        event.preventDefault()
-        document.querySelector(`.todo-${todoId}`).classList.add('opacity-20')
-        await db.collection('todos').doc(todoId).delete()
+        
+        document.querySelector(`.todo-${todoId}`).classList.remove('opacity-20')
+                
       })
+
       document.querySelector('#todo').value = ''
     }
   })
@@ -44,18 +45,26 @@ document.addEventListener('DOMContentLoaded', async function(event) {
 
     document.querySelector('.todos').insertAdjacentHTML('beforeend', `
       <div class="todo-${todoId} py-4 text-xl border-b-2 border-purple-500 w-full">
-        <a href="#" class="done p-2 text-sm bg-green-500 text-white">✓</a>
+        <a href="#" class="done p-2 text-sm bg-green-500 opacity-20 text-white">✓</a>
         ${todoText}
       </div>
     `)
 
     document.querySelector(`.todo-${todoId} .done`).addEventListener('click', async function(event) {
       event.preventDefault()
-      document.querySelector(`.todo-${todoId}`).classList.add('opacity-20')
-      await db.collection('todos').doc(todoId).delete()
+      document.querySelector('.todos').insertAdjacentHTML('beforeend', `
+      <div class="todo-${todoId} py-4 text-xl border-b-2 border-purple-500 w-full">
+        <a href="#" class="done p-2 text-sm bg-green-500  text-white">✓</a>
+        ${todoText}
+      </div>
+    `)
+    
     })
   }
 
   // Step 3: Add code to Step 1 to add todo to Firestore
   // Step 4: Add code to allow completing todos
+
+
+
 })
